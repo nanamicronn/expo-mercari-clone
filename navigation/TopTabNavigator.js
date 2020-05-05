c07ee825f3ecc993b181f799af81b710598e7995
NanamiTanida <nanami_tanida@rebuild-inc.co.jp>
2020-05-05 19:32:09 +0900

    [update] tabbarをカスタマイズしてみたけどやりたいことできない

--- navigation/TopTabNavigator.js
+++ navigation/TopTabNavigator.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-import { Text } from 'react-native';
+import Animated from 'react-native-reanimated';
+import { Text, View, TouchableOpacity } from 'react-native';
 
 const Tab = createMaterialTopTabNavigator();
 
@@ -34,14 +35,70 @@ const Tab5Screen = () => {
   )
 }
 
+function MyTabBar({ state, descriptors, navigation, position }) {
+  return (
+    <View style={{ flexDirection: 'row' }}>
+      {state.routes.map((route, index) => {
+        const { options } = descriptors[route.key];
+        const label =
+          options.tabBarLabel !== undefined
+            ? options.tabBarLabel
+            : options.title !== undefined
+            ? options.title
+            : route.name;
+
+        // state.index(activeなページ)
+        const isFocused = state.index === index;
+
+        const onPress = () => {
+          const event = navigation.emit({
+            type: 'tabPress',
+            target: route.key,
+            canPreventDefault: true,
+          });
+
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name);
+          }
+        };
+
+        const onLongPress = () => {
+          navigation.emit({
+            type: 'tabLongPress',
+            target: route.key,
+          });
+        };
+
+        const inputRange = state.routes.map((_, i) => i);
+
+        const opacity = Animated.interpolate(position, {
+          inputRange,
+          outputRange: inputRange.map(i => (i === index ? 1 : 0)),
+        });
+
+        return (
+          <TouchableOpacity
+            accessibilityRole="button"
+            accessibilityStates={isFocused ? ['selected'] : []}
+            accessibilityLabel={options.tabBarAccessibilityLabel}
+            testID={options.tabBarTestID}
+            onPress={onPress}
+            onLongPress={onLongPress}
+            style={{ flex: 1 }}
+          >
+            <Animated.Text style={{ opacity }}>
+              {label}
+            </Animated.Text>
+          </TouchableOpacity>
+        );
+      })}
+    </View>
+  );
+}
+
 export default () => {
   return (
-    <Tab.Navigator
-      tabBarOptions={{
-        scrollEnabled: true,
-        tabStyle: { margin: 0 }
-      }}
-    >
+    <Tab.Navigator tabBar={props => <MyTabBar {...props} />}>
       <Tab.Screen name="Tab1" component={Tab1Screen} />
       <Tab.Screen name="Tab2" component={Tab2Screen} />
       <Tab.Screen name="Tab3" component={Tab3Screen} />
