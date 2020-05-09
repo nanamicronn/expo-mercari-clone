038e13908c07080e791d43034d0e009d0b04c8c0
NanamiTanida <nanami_tanida@rebuild-inc.co.jp>
2020-05-09 15:20:35 +0900

    [fix] Appの場所かえるのやーめっぴ

--- App.js
+++ App.js
@@ -6,8 +6,8 @@ import { Ionicons } from '@expo/vector-icons';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import BottomTabNavigator from './navigation/BottomTabNavigator';
-import useLinking from './navigation/useLinking';
+import BottomTabNavigator from './src/navigation/BottomTabNavigator';
+import useLinking from './src/navigation/useLinking';
 
 const Stack = createStackNavigator();
 
@@ -29,7 +29,7 @@ export default function App(props) {
         // Load fonts
         await Font.loadAsync({
           ...Ionicons.font,
-          'space-mono': require('../assets/fonts/SpaceMono-Regular.ttf'),
+          'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
         });
       } catch (e) {
         // We might want to provide this error information to an error reporting service
--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import NavigationTestUtils from 'react-navigation/NavigationTestUtils';
 import renderer from 'react-test-renderer';
 
-import App from '../src/App';
+import App from '../App';
 
 jest.mock('expo', () => ({
   AppLoading: 'AppLoading',
